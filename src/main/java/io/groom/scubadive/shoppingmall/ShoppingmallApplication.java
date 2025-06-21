47b26932b001af4fa973f9b9b364e9a7adc4cb32
projectmiluju <dnjsdyd0712@naver.com>
2025-06-22 04:52:03 +0900

    "Gradle 설정과 기본 애플리케이션 구조를 포함한 Spring Boot 프로젝트 초기화

---
+++ src/main/java/io/groom/scubadive/shoppingmall/ShoppingmallApplication.java
@@ -0,0 +1,15 @@
+package io.groom.scubadive.shoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class ShoppingmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallApplication.class, args);
+	}
+
+}
