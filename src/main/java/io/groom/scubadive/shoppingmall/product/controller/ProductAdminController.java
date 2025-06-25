61d8681f0766d6651ad686256fbdeeec0e07e0c5
theo-ooooo <kkw.theo@gmail.com>
2025-06-25 14:22:29 +0900

    feature(product): 상품 관리자 콘트롤러, 유저 콘트롤러 구분 및 기본 셋팅

---
+++ src/main/java/io/groom/scubadive/shoppingmall/product/controller/ProductAdminController.java
@@ -0,0 +1,85 @@
+package io.groom.scubadive.shoppingmall.product.controller;
+
+import io.groom.scubadive.shoppingmall.global.dto.ApiResponseDto;
+import io.groom.scubadive.shoppingmall.product.domain.Product;
+import io.groom.scubadive.shoppingmall.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin/products")
+@RequiredArgsConstructor
+public class ProductAdminController {
+    private final ProductService productService;
+
+
+    /**
+     * 상품 전체 조회(관리자)
+     * @return
+     */
+    @GetMapping()
+    public ApiResponseDto<Void> getProducts() {
+        return null;
+    }
+
+    /**
+     * 상품 단일 조회
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ApiResponseDto<Void> getProduct(@PathVariable Long id) {
+        return null;
+    }
+
+    /**
+     * 상품 추가
+     * @param product
+     * @return
+     */
+    @PostMapping()
+    public ApiResponseDto<Void> createProduct(@RequestBody Product product) {
+        return null;
+    }
+
+    /**
+     * 상품 수정
+     * @param id
+     * @param product
+     * @return
+     */
+    @PatchMapping("/{id}")
+    public ApiResponseDto<Void> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return null;
+    }
+
+    /**
+     * 상품 삭제
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public ApiResponseDto<Void> deleteProduct(@PathVariable Long id) {
+        return null;
+    }
+
+    /**
+     * 상품 상태 변경
+     * @param id
+     * @return
+     */
+    @PatchMapping("/{id}/status")
+    public ApiResponseDto<Void> updateStatus(@PathVariable Long id) {
+        return null;
+    }
+
+    /**
+     * 상품 수량 변경
+     * @param id
+     * @return
+     */
+    @PatchMapping("/{id}/stock")
+    public ApiResponseDto<Void> updateStock(@PathVariable Long id) {
+        return null;
+    }
+}
--- src/main/java/io/groom/scubadive/shoppingmall/product/controller/ProductController.java
+++ src/main/java/io/groom/scubadive/shoppingmall/product/controller/ProductController.java
@@ -1,14 +1,40 @@
 package io.groom.scubadive.shoppingmall.product.controller;
 
+import io.groom.scubadive.shoppingmall.global.dto.ApiResponseDto;
+import io.groom.scubadive.shoppingmall.product.domain.Product;
 import io.groom.scubadive.shoppingmall.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
 
 
+    /**
+     * 상품 전체 리스트 조회
+     * @return
+     */
+    @GetMapping()
+    public ApiResponseDto<Void> getProducts() {
+        return null;
+    }
+
+    /**
+     * 상품 단일 조회
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public ApiResponseDto<Void> getProduct(@PathVariable Long id) {
+        return null;
+    }
 
 }
