d977bbfec9f0b8941161a91fd26a007d3b14a1a6
hyejin kwon <97959501+sunsetkk@users.noreply.github.com>
2025-06-26 23:45:04 +0900

    delete "다른 branch 작업물 삭제"

--- src/main/java/io/groom/scubadive/shoppingmall/member/repository/UserPaidRepository.java
+++
@@ -1,9 +0,0 @@
-package io.groom.scubadive.shoppingmall.memeber.repository;
-
-import io.groom.scubadive.shoppingmall.memeber.domain.UserPaid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPaidRepository extends JpaRepository<UserPaid, Long> {
-    boolean existsByUserId(Long userId);
-    UserPaid findByUserId(Long userId);
-}
