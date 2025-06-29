ee47715c2654997bc4776e8ac032f99075640c04
sunsetkk <khyejin9796@gmail.com>
2025-06-29 18:50:40 +0900

    feat(user): JWT 설정 클래스 및 보안 설정 추가

--- src/main/java/io/groom/scubadive/shoppingmall/global/securirty/JwtProperties.java
+++ src/main/java/io/groom/scubadive/shoppingmall/global/securirty/JwtProperties.java
@@ -2,13 +2,18 @@ package io.groom.scubadive.shoppingmall.global.securirty;
 
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
 @Getter
-@Component
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private String secretKey;
-    private long accessTokenExpiration;
-    private long refreshTokenExpiration;
+    private final String secretKey;
+    private final long accessTokenExpiration;
+    private final long refreshTokenExpiration;
+
+    public JwtProperties(String secretKey, long accessTokenExpiration, long refreshTokenExpiration) {
+        this.secretKey = secretKey;
+        this.accessTokenExpiration = accessTokenExpiration;
+        this.refreshTokenExpiration = refreshTokenExpiration;
+    }
 }
