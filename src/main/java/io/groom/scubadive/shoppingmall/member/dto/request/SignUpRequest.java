cf070c20c51939b6c20a5c8403ff24e13dbcb0de
projectmiluju <dnjsdyd0712@naver.com>
2025-07-02 06:33:18 +0900

    feat(user): SignUpRequest 클래스에 @Setter 추가

--- src/main/java/io/groom/scubadive/shoppingmall/member/dto/request/SignUpRequest.java
+++ src/main/java/io/groom/scubadive/shoppingmall/member/dto/request/SignUpRequest.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class SignUpRequest {
 
     @NotBlank(message = "이메일은 필수입니다.")
