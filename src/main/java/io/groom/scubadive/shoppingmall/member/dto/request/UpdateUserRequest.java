ba8dadd3e6290c73c391b2a30fb6e6eec3a905d6
projectmiluju <dnjsdyd0712@naver.com>
2025-07-02 07:35:49 +0900

    feat(user): UpdateUserRequest에서 비밀번호 및 닉네임 관련 유효성 검사 제거

--- src/main/java/io/groom/scubadive/shoppingmall/member/dto/request/UpdateUserRequest.java
+++ src/main/java/io/groom/scubadive/shoppingmall/member/dto/request/UpdateUserRequest.java
@@ -9,18 +9,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UpdateUserRequest {
 
-    @Size(min = 3, max = 20, message = "닉네임은 3자 이상 20자 이하여야 합니다.")
     private String nickname;
 
     private String phoneNumber;
 
-    @Size(min = 4, max = 30, message = "현재 비밀번호는 4자 이상이어야 합니다.")
     private String currentPassword;
 
-    @Size(min = 4, max = 30, message = "새 비밀번호는 4자 이상이어야 합니다.")
     private String newPassword;
 
-    @Size(min = 4, max = 30)
     private String newPasswordCheck;
 
 }
