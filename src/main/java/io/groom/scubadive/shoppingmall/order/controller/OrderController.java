709c2745c95e6776ad059c264da5a2105b071b6c
projectmiluju <dnjsdyd0712@naver.com>
2025-07-05 04:39:03 +0900

    refactor(order): 사용하지 않는 import문 제거

--- src/main/java/io/groom/scubadive/shoppingmall/order/controller/OrderController.java
+++ src/main/java/io/groom/scubadive/shoppingmall/order/controller/OrderController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
