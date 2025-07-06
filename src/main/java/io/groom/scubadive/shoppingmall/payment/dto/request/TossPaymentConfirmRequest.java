3e7d76a38fbbd026f51f51251f7c36578c7879dc
projectmiluju <dnjsdyd0712@naver.com>
2025-07-06 13:59:53 +0900

    feat(payment): 사용하지 않는 dto 제거

--- src/main/java/io/groom/scubadive/shoppingmall/payment/dto/request/TossPaymentConfirmRequest.java
+++
@@ -1,9 +0,0 @@
-package io.groom.scubadive.shoppingmall.payment.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class TossPaymentConfirmRequest {
-    private String paymentKey;
-    private String orderId;
-}
